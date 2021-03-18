2fb44cf98c5da8e746edc26036720d519b3eb63c
GonzaFalcon <gonzfalcon98@gmail.com>
2021-03-18 13:52:06 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A889126.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+
+            Console.WriteLine("Presione Enter para salir.");
+            Console.ReadLine();
+        }
+    }
+}
